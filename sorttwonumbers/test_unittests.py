6f5ea474f154c3c954d4ab91baf4406b63c03017
Mia Weber <miaweber02@gmail.com>
2024-02-05 00:37:10 +0000

    fixed formatting

--- sorttwonumbers/test_unittests.py
+++ sorttwonumbers/test_unittests.py
@@ -5,16 +5,20 @@ Unit tests for solution.py
 """
 
 import unittest
-import pytest
+
 from sorttwonumbers import sort
+
 
 class TestSort(unittest.TestCase):
     def test1(self) -> None:
-        self.assertFalse(sort(8,2))
+        self.assertFalse(sort(8, 2))
+
     def test2(self) -> None:
-        self.assertTrue(sort(-4,4))
+        self.assertTrue(sort(-4, 4))
+
     def test3(self) -> None:
-        self.assertFalse(sort(-7485,-7485))
+        self.assertFalse(sort(-7485, -7485))
+
 
 if __name__ == "__main__":
     unittest.main()
